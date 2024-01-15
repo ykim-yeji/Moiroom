445b85f46308ec14605172fe25a50f3b155ce044
core <jinjs6@gmail.com>
2024-01-15 13:46:28 +0900

    chore: 모이룸 서버 프로젝트 생성

---
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/MoiroomserverApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.moiroomserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoiroomserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoiroomserverApplication.class, args);
+	}
+
+}
