c12de591bbd853e9e35c53f2af132ca3a0ef14fc
황재언 <hju753@naver.com>
2024-01-17 17:54:09 +0900

    android projected created

---
+++ android/moiroom/app/src/main/java/com/example/moiroom/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.moiroom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+
+class NainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
+
