1849d37fd34f6c443a903c82c0bb8999d9a2c945
김준수 <pittuarez@naver.com>
2024-01-22 17:19:47 +0900

    회원 정보 입력 디자인

--- android/moiroom/app/build.gradle.kts
+++ android/moiroom/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.moiroom"
-        minSdk = 21
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
--- android/moiroom/app/src/main/java/com/example/moiroom/InfoinputActivity.kt
+++ android/moiroom/app/src/main/java/com/example/moiroom/InfoinputActivity.kt
@@ -1,5 +1,6 @@
 package com.example.moiroom
 
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -7,21 +8,23 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.WindowManager
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.BaseAdapter
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import com.example.moiroom.databinding.ActivityInfoinputBinding
 import kotlinx.coroutines.launch
 import retrofit2.create
 
-
 class InfoinputActivity : AppCompatActivity() {
 
-    // xml 레이아웃과 바인딩
+    // 뷰바인딩
     private lateinit var binding: ActivityInfoinputBinding
-    // 사용자 입력 문구 (자기소개)
+    // 사용자가 입력한 자기소개 저장
     private var userInput: String = ""
+    private var textLength: String = "0/30"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,83 +32,69 @@ class InfoinputActivity : AppCompatActivity() {
         binding = ActivityInfoinputBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // retrofit 가져오기
         val globalApplication = application as GlobalApplication
         val apiInterface = globalApplication.retrofit.create(ApiInterface::class.java)
 
-        // 사용자 입력 바인딩
+        binding.textLength.text = textLength
+        // 사용자 입력 자기소개 저장하기
         binding.editText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(charSequence: CharSequence?, start: Int, before: Int, count: Int) {
                 userInput = charSequence.toString()
+                textLength = "${charSequence?.length ?: 0}/30"
+                binding.textLength.text = textLength
             }
-            override fun afterTextChanged(s: Editable?) {
-            }
+            override fun afterTextChanged(s: Editable?) {}
         })
 
-        // GET 요청 보내기 1
-        lifecycleScope.launch {
-            val posts = apiInterface.getPosts()
+        // 지역 찾기 버튼 누르기
+        binding.findInput.setOnClickListener {
+            // GET 요청 보내기 1
+            lifecycleScope.launch {
+                val posts = apiInterface.getPosts()
 
-            val spinnerAdapter = ArrayAdapter(
-                this@InfoinputActivity,
-                android.R.layout.simple_spinner_item,
-                posts.map { it.title }
-            )
-            binding.spinnerTitles.adapter = spinnerAdapter
+                // Posts를 선택하는 다이얼로그
+                showItemSelectionDialog("Choose a title", posts.map { it.title }) { selectedTitle ->
+                    val selectedPosts = posts.find { it.title == selectedTitle }
+                    if (selectedPosts != null) {
+                        // GET 요청 보내기 2
+                        lifecycleScope.launch {
+                            val posts2 = apiInterface.getAnother()
 
-            binding.spinnerTitles.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(
-                    parent: AdapterView<*>?,
-                    view: View?,
-                    position: Int,
-                    id: Long
-                ) {
-//                    binding.textSelectedTitle.text = "선택된 지역 : ${posts[position].id}"
-                    val selectedTitle = posts[position].id
+                            // Posts2를 선택하는 다이얼로그
+                            showItemSelectionDialog("Choose a detail", posts2.map { it.title }) { selectedDetail ->
+                                val selectedPosts2 = posts2.find { it.title == selectedDetail }
+                                if (selectedPosts2 != null) {
+                                    binding.findInput.text =
+                                            "${selectedPosts.title}, ${selectedPosts2.title}"
 
-                    // GET 요청 보내기 2
-                    lifecycleScope.launch {
-                        val posts2 = apiInterface.getAnother()
+                                    // 확인 버튼
+                                    binding.saveButton.setOnClickListener {
+                                        println("UserInput: ${selectedPosts.id}, ${selectedPosts2.id}, $userInput")
+                                        // POST 요청 구현 필요
 
-                        val spinnerAdapter2 = ArrayAdapter(
-                            this@InfoinputActivity,
-                            android.R.layout.simple_spinner_item,
-                            posts2.map { it.title }
-                        )
-                        binding.spinnerDetails.adapter = spinnerAdapter2
-                        binding.spinnerDetails.visibility = View.VISIBLE
-
-                        binding.spinnerDetails.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                            override fun onItemSelected(
-                                parentDetails: AdapterView<*>?,
-                                viewDetails: View?,
-                                positionDetails: Int,
-                                idDetails: Long
-                            ) {
-                                val selectedDetail = posts2[positionDetails].id
-
-                                binding.textSelectedTitle.text = "선택된 지역 : ${posts[position].id}, ${posts2[positionDetails].id}"
-
-                                // 확인 버튼
-                                binding.saveButton.setOnClickListener {
-                                    println("UserInput: ${posts[position].id}, ${posts2[positionDetails].id}, $userInput")
-                                    // POST 요청 구현 필요
-
-                                    // 액티비티 이동
-                                    val intent = Intent(this@InfoinputActivity, NaviActivity::class.java)
-                                    startActivity(intent)
+                                        // 액티비티 이동
+                                        val intent = Intent(this@InfoinputActivity, NaviActivity::class.java)
+                                        startActivity(intent)
+                                    }
                                 }
-                            }
-                            override fun onNothingSelected(parent: AdapterView<*>?) {
                             }
                         }
                     }
                 }
-                override fun onNothingSelected(parent: AdapterView<*>?) {
-                }
             }
         }
+    }
+
+    private fun showItemSelectionDialog(title: String, items: List<String>, onItemSelected: (String) -> Unit) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+        builder.setItems(items.toTypedArray()) { _, which ->
+            val selectedTitle = items[which]
+            onItemSelected(selectedTitle)
+        }
+
+        val alertDialog = builder.create()
+        alertDialog.show()
     }
 }
