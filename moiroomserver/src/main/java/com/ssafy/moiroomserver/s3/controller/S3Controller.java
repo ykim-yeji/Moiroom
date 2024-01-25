b707735a6d55106d055ed0434f35e444f67f1dfe
김예지 <ilice0403@naver.com>
2024-01-25 17:21:48 +0900

    style: S3Controller, S3Service í 필요없는 코드 삭제 및 url 수정

--- moiroomserver/src/main/java/com/ssafy/moiroomserver/s3/controller/S3Controller.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/s3/controller/S3Controller.java
@@ -6,6 +6,7 @@ import com.ssafy.moiroomserver.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,10 +17,9 @@ public class S3Controller {
 
     private final S3Service s3Service;
 
-    @PostMapping("/upload")
-    public ApiResponse<?> upload(MultipartFile file) {
+    @PostMapping("/upload/profileImage")
+    public ApiResponse<?> upload(@RequestParam("memberProfileImage") MultipartFile file) {
         String memberProfileImageUrl = s3Service.uploadProfileImage(file);
-        System.out.println("프로필 사진 url: " + memberProfileImageUrl);
 
         return ApiResponse.success(SuccessCode.UPLOAD_PROFILE_IMAGE, memberProfileImageUrl);
     }
