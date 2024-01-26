9e385c2ffdafa0cc5867d15b78bfb7af4f4fa51b
김예지 <ilice0403@naver.com>
2024-01-26 16:15:47 +0900

    fix: PageController  showPriprivacy -> vacyPage 메소드 이름 변경

--- moiroomserver/src/main/java/com/ssafy/moiroomserver/page/PageController.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/page/PageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
 
     @GetMapping("/moiroom/privacy")
-    public String privacy() {
+    public String showPrivacyPage() {
         return "privacy";
     }
 }
