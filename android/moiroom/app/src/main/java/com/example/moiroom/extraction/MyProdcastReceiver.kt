8052939c7862b0887b3eebd200f4e7a7a784324f
황재언 <hju753@naver.com>
2024-01-26 17:57:50 +0900

    test: 인스타 엑세스 토큰

---
+++ android/moiroom/app/src/main/java/com/example/moiroom/extraction/MyProdcastReceiver.kt
@@ -0,0 +1,19 @@
+package com.example.moiroom.extraction
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+
+class MyProdcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        // "myapp://" 스킴을 처리하는 로직을 여기에 추가
+        if (intent != null && Intent.ACTION_VIEW == intent.action) {
+            val data: Uri? = intent.data
+            if (data != null && "moiroom://instagram" == data.scheme) {
+                // "myapp://" 스킴으로 들어온 경우에 대한 처리
+                // 예: 특정 화면으로 이동하거나 특정 동작 수행
+            }
+        }
+    }
+}
