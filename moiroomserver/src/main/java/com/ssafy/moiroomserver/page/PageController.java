14839376f805345735a091090021771864a3358e
김예지 <ilice0403@naver.com>
2024-01-26 16:15:05 +0900

    feat: PageController 추가 - pri의 privacy, l 화면 띄우는 컨트롤러 추가

---
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/page/PageController.java
@@ -0,0 +1,13 @@
+package com.ssafy.moiroomserver.page;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/moiroom/privacy")
+    public String privacy() {
+        return "privacy";
+    }
+}
