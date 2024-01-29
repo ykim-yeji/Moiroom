195e0bc45c5cfb86b8a62695a5127ecadeb962c2
core <jinjs6@gmail.com>
2024-01-29 17:38:39 +0900

    feat: OAuth2 서비스를 활용한 카카오 로그인 로직 구현

--- moiroomserver/src/main/java/com/ssafy/moiroomserver/oauth/service/OAuthService.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/oauth/service/OAuthService.java
@@ -1,57 +1,76 @@
 package com.ssafy.moiroomserver.oauth.service;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import com.ssafy.moiroomserver.oauth.entity.KakaoMember;
+import com.ssafy.moiroomserver.oauth.repository.OAuthRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 @Slf4j
-public class OauthService {
+public class OAuthService extends DefaultOAuth2UserService {
 
-    public String getKakaoAccessToken(String code) {
+    @Autowired
+    OAuthRepository oAuthRepository;
 
-        String accessToken = null;
-        String requestURL = "https://kauth.kakao.com/oauth/token";
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        System.out.println("OAuthService.loadUser");
+        // email, oauthType 호출
+        Map<String, Object> attributes = super.loadUser(userRequest).getAttributes();
+        log.info("ATTR INFO : {}", attributes.toString());
 
-        try {
-            URL url = new URL(requestURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
+        String email = null;
+        String kakaoId = null;
+        String oauthType = userRequest.getClientRegistration().getRegistrationId();
 
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-            StringBuilder input = new StringBuilder();
-            input.append("grant_type=authorization_code")
-                    .append("&client_id=").append("cfd8bf207f95e154c8f064581f71f655")
-                    .append("&redirect_uri=").append("http://127.0.0.1:8080/oauth/kakao")
-                    .append("&code=").append(code);
-            bw.write(input.toString());
-            bw.flush();
+        OAuth2User user2 = super.loadUser(userRequest);
 
-            Integer responseCode = conn.getResponseCode();
-            log.info("responseCode: {}", responseCode); // 응답 코드 200인지 확인하기
-
-            // 요청 데이터 얻어오기
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line = null;
-            StringBuilder output = new StringBuilder();
-
-            while ((line = br.readLine()) != null) {
-                output.append(line);
-            }
-
-            // output에 refreshToken, accessToken 등의 정보가 존재하고 있는 상태.
-            JsonElement element = JsonParser.parseString(output.toString());
-            accessToken = element.getAsJsonObject().get("access_token").getAsString();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        // oauth 타입에 따라 데이터가 다르기에 분기
+        if("kakao".equals(oauthType.toLowerCase())) {
+            // kakao는 kakao_account 내에 email이 존재함.
+            kakaoId = attributes.get("id").toString();
+            email = ((Map<String, Object>) attributes.get("kakao_account")).get("email").toString();
+        }
+        else if("google".equals(oauthType.toLowerCase())) {
+            email = attributes.get("email").toString();
+        }
+        else if("naver".equals(oauthType.toLowerCase())) {
+            // naver는 response 내에 email이 존재함.
+            email = ((Map<String, Object>) attributes.get("response")).get("email").toString();
         }
 
-        return accessToken;
+        // User 존재여부 확인 및 없으면 생성
+        if(getKakaoMemberByKakaoId(kakaoId) == null){
+            log.info("kakao({}) NOT EXISTS. REGISTER", kakaoId, oauthType);
+            KakaoMember member = new KakaoMember();
+            member.setKakaoMemberId(UUID.randomUUID().toString()); // 수정해야 할 듯
+            member.setKakaoId(kakaoId);
+            member.setEmail(email);
+            member.setOauthType(oauthType);
+
+            save(member);
+        }
+        return super.loadUser(userRequest);
+    }
+
+    // 저장, 조회만 수행. 기타 예외처리 및 다양한 로직은 연습용이므로
+    public void save(KakaoMember kakaoMember) {
+        oAuthRepository.save(kakaoMember);
+    }
+
+    public KakaoMember getKakaoMemberByKakaoId(String kakaoId) {
+        return oAuthRepository.findByKakaoId(kakaoId).orElse(null);
+    }
+
+    public KakaoMember getKakaoMemberByEmailAndOAuthType(String email, String oauthType) {
+        return oAuthRepository.findByEmailAndOauthType(email, oauthType).orElse(null);
     }
 }
