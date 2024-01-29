1b0322819b185725f3e4ae009c2fe285abd8463b
core <jinjs6@gmail.com>
2024-01-29 17:37:47 +0900

    feat: 카카오 로그인 서비스 화면을 위한 컨트롤러 코드 작성

--- moiroomserver/src/main/java/com/ssafy/moiroomserver/oauth/controller/OAuthController.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/oauth/controller/OAuthController.java
@@ -1,27 +1,43 @@
 package com.ssafy.moiroomserver.oauth.controller;
 
-import com.ssafy.moiroomserver.oauth.service.OauthService;
+import com.ssafy.moiroomserver.oauth.service.OAuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
-public class OauthController {
+public class OAuthController {
 
-    private final OauthService oauthService;
+    private final OAuthService oauthService;
 
-    @ResponseBody
-    @GetMapping("/oauth/kakao")
-    public String kakaoLogin(@RequestParam String code) {
-        log.info("code: " + code);
-        String accessToken = oauthService.getKakaoAccessToken(code);
+//    @ResponseBody
+//    @GetMapping("/oauth/kakao")
+//    public String kakaoLogin(@RequestParam String code) {
+//        log.info("code: " + code);
+//        String accessToken = oauthService.getKakaoAccessToken(code);
+//
+//        // 카카오 엑세스 토큰을 바탕으로 회원 정보를 가져오기
+//        return "success";
+//    }
 
-        // 카카오 엑세스 토큰을 바탕으로 회원 정보를 가져오기
-        return "success";
+    /**
+     * 로그인 페이지
+     * @return
+     */
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    /**
+     * 메인 페이지
+     * @return
+     */
+    @GetMapping("/")
+    public String main() {
+        return "main";
     }
 }
