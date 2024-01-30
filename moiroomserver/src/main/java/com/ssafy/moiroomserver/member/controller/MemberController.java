b933a8597361919993b19593ea27a8b78d294b2b
김예지 <ilice0403@naver.com>
2024-01-30 11:33:34 +0900

    feat: 회원정보일부수정 dto 생성

--- moiroomserver/src/main/java/com/ssafy/moiroomserver/member/controller/MemberController.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/member/controller/MemberController.java
@@ -1,14 +1,21 @@
 package com.ssafy.moiroomserver.member.controller;
 
+import com.ssafy.moiroomserver.global.dto.ApiResponse;
 import com.ssafy.moiroomserver.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin("*")
 @RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
 
     private final MemberService memberService;
+
+    public ApiResponse<?> modifyMemberInfo() {
+        return null;
+    }
 }
