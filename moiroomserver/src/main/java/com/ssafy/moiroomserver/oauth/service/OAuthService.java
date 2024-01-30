e9fa165eabd7176860222911958b702f4e68701a
core <jinjs6@gmail.com>
2024-01-30 15:48:52 +0900

    feat: 카카오 로그인을 통한 회원 닉네임, 이미지 정보 db 저장 로직 추가

--- moiroomserver/src/main/java/com/ssafy/moiroomserver/oauth/service/OAuthService.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/oauth/service/OAuthService.java
@@ -1,6 +1,7 @@
 package com.ssafy.moiroomserver.oauth.service;
 
 import com.ssafy.moiroomserver.oauth.dto.KakaoAccountDto;
+import com.ssafy.moiroomserver.oauth.dto.KakaoProfile;
 import com.ssafy.moiroomserver.oauth.entity.KakaoMember;
 import com.ssafy.moiroomserver.oauth.repository.OAuthRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -51,8 +52,14 @@ public class OAuthService extends DefaultOAuth2UserService {
 
     private void joinKakaoMember(String oauthType, KakaoAccountDto dto) {
         log.info("kakao({}) NOT EXISTS. REGISTER", dto.getKakaoId(), oauthType);
+        KakaoProfile kakaoProfile = new KakaoProfile();
+        kakaoProfile.setNickname(dto.getKakaoProfile().getNickname());
+        kakaoProfile.setThumbnailImageUrl(dto.getKakaoProfile().getThumbnailImageUrl());
+        kakaoProfile.setProfileImageUrl(dto.getKakaoProfile().getProfileImageUrl());
+
         KakaoMember member = new KakaoMember();
-        member.setKakaoMemberId(UUID.randomUUID().toString()); // 수정해야 할 듯
+        member.setId(UUID.randomUUID().toString()); // 수정해야 할 듯
+        member.setKakaoProfile(kakaoProfile);
         member.setKakaoId(dto.getKakaoId());
         member.setEmail(dto.getEmail());
         member.setName(dto.getName());
@@ -67,7 +74,16 @@ public class OAuthService extends DefaultOAuth2UserService {
     }
 
     private static void setKakaoAccount(KakaoAccountDto dto, Map<String, Object> attributes) {
+        // kakaoProfile에 정보 입력하고 dto에 담아주기
+        KakaoProfile kakaoProfile = new KakaoProfile();
+        Map<String, Object> map = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) map.get("profile");
+        kakaoProfile.setNickname((String) profile.get("nickname"));
+        kakaoProfile.setThumbnailImageUrl((String) profile.get("thumbnail_image_url")); // 프로필 미리보기 이미지 url
+        kakaoProfile.setProfileImageUrl((String) profile.get("profile_image_url")); // 프로필 사진 url
+
         dto.setKakaoId(attributes.get("id").toString());
+        dto.setKakaoProfile(kakaoProfile);
         dto.setEmail(((Map<String, Object>) attributes.get("kakao_account")).get("email").toString());
         dto.setName(((Map<String, Object>) attributes.get("kakao_account")).get("name").toString());
         dto.setAgeRange(((Map<String, Object>) attributes.get("kakao_account")).get("age_range").toString());
