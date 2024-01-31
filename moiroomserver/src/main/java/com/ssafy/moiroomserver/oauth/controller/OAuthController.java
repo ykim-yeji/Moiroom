c8d8f76ee447524103f3087ae66aba2b093b76f1
core <jinjs6@gmail.com>
2024-01-31 15:09:47 +0900

    feat: 불필요한 로직 제거 및 accessToken, refreshToken 추출 로직 추가

--- moiroomserver/src/main/java/com/ssafy/moiroomserver/oauth/controller/OAuthController.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/oauth/controller/OAuthController.java
@@ -3,6 +3,8 @@ package com.ssafy.moiroomserver.oauth.controller;
 import com.ssafy.moiroomserver.oauth.service.OAuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,17 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class OAuthController {
 
-    private final OAuthService oauthService;
-
-//    @ResponseBody
-//    @GetMapping("/oauth/kakao")
-//    public String kakaoLogin(@RequestParam String code) {
-//        log.info("code: " + code);
-//        String accessToken = oauthService.getKakaoAccessToken(code);
-//
-//        // 카카오 엑세스 토큰을 바탕으로 회원 정보를 가져오기
-//        return "success";
-//    }
+    private final OAuthService oAuthService;
 
     /**
      * 로그인 페이지
@@ -37,7 +29,24 @@ public class OAuthController {
      * @return
      */
     @GetMapping("/")
-    public String main() {
+    public String main(OAuth2AuthenticationToken authenticationToken) {
+        // OAuth2 로그인 사용자 정보 획득
+        OAuth2User principal = authenticationToken.getPrincipal();
+        String registrationId = authenticationToken.getAuthorizedClientRegistrationId();
+        String userName = principal.getName(); // 혹은 필요에 따라 다른 식별자 사용
+
+        //accessToken 추출
+        String accessToken = oAuthService.getAccessToken(registrationId, userName);
+
+        // refreshToken 추출
+        String refreshToken = oAuthService.getRefreshToken(registrationId, userName);
+
+        //accessToken 로깅
+        System.out.println("accessToken = " + accessToken);
+
+        // refreshToken 로깅
+        System.out.println("refreshToken = " + refreshToken);
+
         return "main";
     }
 }
