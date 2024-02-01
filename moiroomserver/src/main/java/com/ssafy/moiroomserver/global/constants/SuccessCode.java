ff1d9ee301b7c5ef293a638601c1e6017f72cb60
core <jinjs6@gmail.com>
2024-02-01 11:31:16 +0900

    feat: 회원 정보 추가 api 구현

--- moiroomserver/src/main/java/com/ssafy/moiroomserver/global/constants/SuccessCode.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/global/constants/SuccessCode.java
@@ -14,7 +14,8 @@ public enum SuccessCode {
     UPLOAD_IMAGE(OK, "이미지 업로드 성공"),
     //회원
     UPLOAD_PROFILE_IMAGE(OK, "프로필 사진 업로드 성공"),
-    MODIFY_MEMBER_INFO(OK, "회원 정보 수정 성공");
+    MODIFY_MEMBER_INFO(OK, "회원 정보 수정 성공"),
+    ADD_MEMBER_INFO(CREATED, "회원 정보 추가 성공");
 
     private final HttpStatus status;
     private final String message;
--- moiroomserver/src/main/java/com/ssafy/moiroomserver/member/service/MemberService.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/member/service/MemberService.java
@@ -1,8 +1,11 @@
 package com.ssafy.moiroomserver.member.service;
 
+import com.ssafy.moiroomserver.member.dto.AddMemberDto;
 import com.ssafy.moiroomserver.member.dto.MemberInfo;
 
 public interface MemberService {
 
     void modifyMemberInfo(MemberInfo.ModifyRequest infoModifyRequest);
+
+    void addMember(AddMemberDto dto);
 }
