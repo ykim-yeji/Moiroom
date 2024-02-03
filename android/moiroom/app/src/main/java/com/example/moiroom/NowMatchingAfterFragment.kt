7c7c3bb09b191620e9e82a8b32663a788261dc89
정성현 <gkgktjdgusdl@naver.com>
2024-02-03 23:05:33 +0900

    feat : 백업 코드

--- android/moiroom/app/src/main/java/com/example/moiroom/NowMatchingAfterFragment.kt
+++ android/moiroom/app/src/main/java/com/example/moiroom/NowMatchingAfterFragment.kt
@@ -1,6 +1,7 @@
 package com.example.moiroom
 
 //import OnBackButtonClickListener
+import android.content.Context
 import android.os.Bundle
 import android.os.Looper
 import android.system.Os.remove
@@ -13,6 +14,7 @@ import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.example.moiroom.adapter.CardAdapter
 import com.example.moiroom.adapter.CardDetailPagerAdapter
@@ -166,4 +168,3 @@ class NowMatchingAfterFragment : Fragment(), OnBackButtonClickListener {
         binding.viewPager2.visibility = View.GONE
     }
 }
-
