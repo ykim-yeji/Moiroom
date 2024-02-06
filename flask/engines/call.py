c66b6b7c317d1bf56ad292f1b830547ea006c5ae
전새벽 <wjswwkd@naver.com>
2024-02-06 17:14:12 +0900

    feat: 통화 내역 분석

--- flask/engines/call.py
+++ flask/engines/call.py
@@ -26,18 +26,14 @@ def calc(json_arr):
     for phone_number, start_time, duration, owner_name in zip(phone_numbers, call_start_times, call_durations,
                                                               owner_names):
         if phone_number not in data_by_phone_number:
-            data_by_phone_number[phone_number] = {'times': [], 'durations': [], 'owner_names': []}
+            data_by_phone_number[phone_number] = {'times': [], 'durations': [], 'owner_name': owner_name}
         data_by_phone_number[phone_number]['times'].append(start_time)
         data_by_phone_number[phone_number]['durations'].append(duration)
-        data_by_phone_number[phone_number]['owner_names'].append(owner_name)
+
 
     # 통화 시작 시간에 따른 통화 시간 시각화 (날짜 제외, 시간만 표시)
-    parents_total_duration = 0
     for phone_number, data in data_by_phone_number.items():
-        plt.scatter(data['times'], data['durations'], label=data['owner_names'][0])
-        parents_total_duration += sum(
-            [data['durations'][i] for i in range(len(phone_numbers))
-             if ('엄마' in data['owner_names'][0]) or ('아빠' in data['owner_names'][0])])
+        plt.scatter(data['times'], data['durations'], label=data['owner_name'])
 
     plt.xlabel('통화 시작 시간 (시간)')
     plt.ylabel('통화 시간 (초)')
@@ -45,6 +41,11 @@ def calc(json_arr):
     plt.legend()
     plt.xticks(rotation=45)
     plt.show()
+
+    parents_total_duration = 0
+    for phone_number, data in data_by_phone_number.items():
+        if '엄마' in data['owner_name'] or '아빠' in data['owner_name']:
+            parents_total_duration += sum([data['durations'][i] for i in range(len(data['durations']))])
 
     data_by_phone_number['parents_total_duration'] = parents_total_duration
     return data_by_phone_number
