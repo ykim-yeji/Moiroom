1f8880b79e6ea6cddd4aa3de2da77f7a0a537b0b
core <jinjs6@gmail.com>
2024-02-07 16:12:31 +0900

    feat: 회원 상세 정보 조회 관련 dto 생성

---
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/member/dto/Interest.java
@@ -0,0 +1,11 @@
+package com.ssafy.moiroomserver.member.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Interest {
+    private String interestName;
+    private Integer interestPercent;
+}
