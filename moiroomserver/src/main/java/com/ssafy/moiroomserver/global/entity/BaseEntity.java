6eeb45074beecbf3c65a29badd9b4a792e56ae10
ykim-yeji <ykim5867@gmail.com>
2024-02-09 02:17:42 +0900

    feat: application-dev.yml UTC -> Asia/Seoul 타임존 변경

--- moiroomserver/src/main/java/com/ssafy/moiroomserver/global/entity/BaseEntity.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/global/entity/BaseEntity.java
@@ -8,17 +8,17 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @CreatedDate
     @Column(name = "created_at", updatable = false, nullable = false)
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedAt;
 }
