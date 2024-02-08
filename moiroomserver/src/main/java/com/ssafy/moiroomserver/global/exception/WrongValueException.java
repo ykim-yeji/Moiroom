128449cc99b21f3b4214c3e523c17c0a2eb19bc4
김예지 <ilice0403@naver.com>
2024-02-08 09:31:21 +0900

    feat: GlobalExceptionHandlerì WrongValueException 처리 메소드 추가

--- moiroomserver/src/main/java/com/ssafy/moiroomserver/global/exception/WrongValueException.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/global/exception/WrongValueException.java
@@ -1,7 +1,9 @@
 package com.ssafy.moiroomserver.global.exception;
 
 import com.ssafy.moiroomserver.global.constants.ErrorCode;
+import lombok.Getter;
 
+@Getter
 public class WrongValueException extends RuntimeException {
 
 	private final ErrorCode code;
