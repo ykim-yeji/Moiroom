9c58b8100889c4a5cad703899239188fbd2964b3
ykim-yeji <ykim5867@gmail.com>
2024-02-10 19:08:50 +0900

    style: 출력 임시

--- moiroomserver/src/main/java/com/ssafy/moiroomserver/global/kakao/KakaoService.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/global/kakao/KakaoService.java
@@ -18,14 +18,19 @@ public class KakaoService {
     public boolean validateToken(String accessToken) {
         System.out.println("TEST 7 - 에러");
         RestTemplate template = new RestTemplate();
+        System.out.println("TEST 8 - 에러");
         HttpHeaders headers = new HttpHeaders();
+        System.out.println("TEST 9 - 에러");
         headers.set("Authorization", "Bearer " + accessToken);
+        System.out.println("TEST 10 - 에러");
         HttpEntity<?> entity = new HttpEntity<>(headers);
+        System.out.println("TEST 11 - 에러");
 
         try {
+            System.out.println("TEST 12 - 에러");
             ResponseEntity<String> response
                     = template.exchange(INFO_URL, HttpMethod.GET, entity, String.class);
-            System.out.println("TEST 8 - 에러");
+            System.out.println("TEST 13 - 에러");
             System.out.println("response.getStatusCode().is2xxSuccessful() : " + response.getStatusCode().is2xxSuccessful());
             return response.getStatusCode().is2xxSuccessful();
         } catch (Exception e) {
