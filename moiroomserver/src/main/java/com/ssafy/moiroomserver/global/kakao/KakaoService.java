c27cb77eceed768296f3908593997491479ccfe8
core <jinjs6@gmail.com>
2024-02-10 12:02:44 +0900

    chore: 필요 없는 코드 제거

--- moiroomserver/src/main/java/com/ssafy/moiroomserver/global/kakao/KakaoService.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/global/kakao/KakaoService.java
@@ -1,7 +1,6 @@
 package com.ssafy.moiroomserver.global.kakao;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.moiroomserver.global.constants.ErrorCode;
 import com.ssafy.moiroomserver.global.exception.NoExistException;
