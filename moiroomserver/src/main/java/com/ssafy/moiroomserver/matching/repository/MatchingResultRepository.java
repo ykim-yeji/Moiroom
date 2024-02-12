06f0e11a7ad65984dedb4efecf83d11946078712
ykim-yeji <ykim5867@gmail.com>
2024-02-13 00:48:17 +0900

    feat: MatchingResultRepositoryìì, , 조건 미달 매칭 결과 테이블 데이터 매월 1일 삭제 스케줄러 쿼리 작성

--- moiroomserver/src/main/java/com/ssafy/moiroomserver/matching/repository/MatchingResultRepository.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/matching/repository/MatchingResultRepository.java
@@ -3,11 +3,14 @@ package com.ssafy.moiroomserver.matching.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ssafy.moiroomserver.matching.entity.MatchingResult;
+
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface MatchingResultRepository extends JpaRepository<MatchingResult, Long> {
@@ -25,15 +28,19 @@ public interface MatchingResultRepository extends JpaRepository<MatchingResult, 
 	)
 	Page<MatchingResult> findMatchingResultByMemberId(@Param("id") Long memberId, Pageable pageable);
 
+	@Transactional
+	@Modifying(clearAutomatically = true)
 	@Query(
-		"DELETE FROM MatchingResult mr "
-		+ "WHERE mr IN ( "
-			+ "SELECT MatchingResult FROM MatchingResult mr "
-			+ "JOIN Member m1 ON mr.memberOneId = m1.memberId "
-			+ "JOIN Member m2 ON mr.memberTwoId = m2.memberId "
-			+ "WHERE m1.roommateSearchStatus = 0 OR m2.roommateSearchStatus = 0 "
-			+ "OR m1.accountStatus != 1 OR m2.accountStatus != 1 "
-			+ "OR m1.metropolitanId != m2.metropolitanId OR m1.cityId != m2.cityId"
+		"DELETE FROM MatchingResult mrt "
+		+ "WHERE mrt.matchingResultId IN ( "
+			+ "SELECT tmp.matchingResultId FROM ( "
+				+ "SELECT mr.matchingResultId AS matchingResultId FROM MatchingResult mr "
+				+ "JOIN Member m1 ON mr.memberOneId = m1.memberId "
+				+ "JOIN Member m2 ON mr.memberTwoId = m2.memberId "
+				+ "WHERE m1.roommateSearchStatus = 0 OR m2.roommateSearchStatus = 0 "
+				+ "OR m1.accountStatus != 1 OR m2.accountStatus != 1 "
+				+ "OR m1.metropolitanId != m2.metropolitanId OR m1.cityId != m2.cityId "
+			+ ") tmp "
 		+ ")"
 	)
 	void deleteMatchingResultAfterPeriod();
--- moiroomserver/src/main/java/com/ssafy/moiroomserver/matching/service/impl/MatchingSchedulerServiceImpl.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/matching/service/impl/MatchingSchedulerServiceImpl.java
@@ -1,7 +1,5 @@
 package com.ssafy.moiroomserver.matching.service.impl;
 
-import java.time.LocalDateTime;
-
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
