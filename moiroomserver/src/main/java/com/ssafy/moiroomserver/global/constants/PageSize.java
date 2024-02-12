100f6f4726ba30f16ab1d33fc307869c813a4fc1
core <jinjs6@gmail.com>
2024-02-12 17:24:12 +0900

    feat: 채팅방 목록 조회 기능 구현

--- moiroomserver/src/main/java/com/ssafy/moiroomserver/global/constants/PageSize.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/global/constants/PageSize.java
@@ -3,4 +3,5 @@ package com.ssafy.moiroomserver.global.constants;
 public class PageSize {
 
 	public static final int MATCHING_ROOMMATE_LIST_SIZE = 10;
+	public static final int GET_CHAT_ROOM_LIST_SIZE = 10;
 }
