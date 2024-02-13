477a89f929dcc88371bbd9bc8f85036741f584e9
core <jinjs6@gmail.com>
2024-02-13 09:15:19 +0900

    chore: 어노테이션 제거

--- moiroomserver/src/main/java/com/ssafy/moiroomserver/chat/dto/ChatMessageReq.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/chat/dto/ChatMessageReq.java
@@ -1,25 +1,21 @@
 package com.ssafy.moiroomserver.chat.dto;
 
-import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ChatMessageReq {
 
-    @Nullable
     private Long senderId;
 
-    @Nullable
     private Long receiverId;
 
-    @Nullable
     private Long roomId;
 
-    @NotBlank
     private String message;
 }
