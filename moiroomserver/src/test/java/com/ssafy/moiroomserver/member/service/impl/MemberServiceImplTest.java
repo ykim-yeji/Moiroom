a02881ac6426efa85b121997284fe2db8725e121
core <jinjs6@gmail.com>
2024-02-14 20:10:33 +0900

    test: 테스트 환경 설정 재진행

---
+++ moiroomserver/src/test/java/com/ssafy/moiroomserver/member/service/impl/MemberServiceImplTest.java
@@ -0,0 +1,38 @@
+package com.ssafy.moiroomserver.member.service.impl;
+
+import com.ssafy.moiroomserver.member.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+class MemberServiceImplTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void modifyMemberInfo() {
+    }
+
+    @Test
+    void login() {
+
+    }
+
+    @Test
+    void modifyMemberToken() {
+    }
+
+    @Test
+    void getMemberById() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void getMemberInfoDetail() {
+    }
+}
