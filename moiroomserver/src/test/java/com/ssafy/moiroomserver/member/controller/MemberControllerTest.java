0b41200d72dbae2c171995360e4de7ee7b2871f2
core <jinjs6@gmail.com>
2024-02-14 19:00:58 +0900

    test: MemberController 테스트 코드 파일 생성

---
+++ moiroomserver/src/test/java/com/ssafy/moiroomserver/member/controller/MemberControllerTest.java
@@ -0,0 +1,36 @@
+package com.ssafy.moiroomserver.member.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberControllerTest {
+
+    @Test
+    void modifyMemberInfo() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void modifyMemberToken() {
+    }
+
+    @Test
+    void getMemberById() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void addCharacteristic() {
+    }
+
+    @Test
+    void getMemberInfoDetail() {
+    }
+}
