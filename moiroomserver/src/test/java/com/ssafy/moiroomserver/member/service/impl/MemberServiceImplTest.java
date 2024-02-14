ad483d626073132e93e86e455364cb6662fff786
core <jinjs6@gmail.com>
2024-02-14 21:01:16 +0900

    test: 로그인 테스트 코드 작성

--- moiroomserver/src/test/java/com/ssafy/moiroomserver/member/service/impl/MemberServiceImplTest.java
+++ moiroomserver/src/test/java/com/ssafy/moiroomserver/member/service/impl/MemberServiceImplTest.java
@@ -1,6 +1,9 @@
 package com.ssafy.moiroomserver.member.service.impl;
 
+import com.ssafy.moiroomserver.member.dto.AddMemberDto;
+import com.ssafy.moiroomserver.member.entity.Member;
 import com.ssafy.moiroomserver.member.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -16,8 +19,40 @@ class MemberServiceImplTest {
     }
 
     @Test
+    @Transactional
     void login() {
+        //Given
+        AddMemberDto dto = new AddMemberDto(
+                12345L,
+                "kakao",
+                "nicknameTest1",
+                "imageUrlTest1",
+                "1999",
+                "0613",
+                "nameTest1",
+                "male",
+                "accessToken1",
+                "refreshToken1"
+        );
 
+        //When
+        Member member = new Member();
+        member.setSocialId(dto.getSocialId());
+        member.setProvider(dto.getProvider());
+        member.setNickname(dto.getNickname());
+        member.setImageUrl(dto.getImageUrl());
+        member.setBirthyear(dto.getBirthyear());
+        member.setBirthday(dto.getBirthday());
+        member.setName(dto.getName());
+        member.setGender(dto.getGender());
+        member.setAccessToken(dto.getAccessToken());
+        member.setRefreshToken(dto.getRefreshToken());
+
+        memberRepository.save(member);
+
+        //Then
+        Assertions.assertThat(memberRepository.findMemberBySocialIdAndProvider(dto.getSocialId(), dto.getProvider()))
+                .isNull();
     }
 
     @Test
