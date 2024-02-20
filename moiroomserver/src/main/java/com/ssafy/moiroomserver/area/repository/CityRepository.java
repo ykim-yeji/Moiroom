3e4cf1bd51416b42df8d7d5b35acfe0ac9ebf023
core <jinjs6@gmail.com>
2024-02-20 09:24:03 +0900

    fix: repository dto 명칭 변경으로 스프링 부트 실행되지 않는 오류 해결

--- moiroomserver/src/main/java/com/ssafy/moiroomserver/area/repository/CityRepository.java
+++ moiroomserver/src/main/java/com/ssafy/moiroomserver/area/repository/CityRepository.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
 
-    @Query("select new com.ssafy.moiroomserver.area.dto.GetCity(c.cityId, c.metropolitan.metropolitanId, c.name) " +
+    @Query("select new com.ssafy.moiroomserver.area.dto.GetCityResponse(c.cityId, c.metropolitan.metropolitanId, c.name) " +
             "from City c where c.metropolitan.metropolitanId = :metropolitanId")
     List<GetCityResponse> findCitiesByMetropolitanId(Long metropolitanId);
     @Query("SELECT name FROM City WHERE cityId = :cityId")
